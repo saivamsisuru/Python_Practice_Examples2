289a633cdf5fbaf649328b88f5afb5d100d32455
vamsi <saivamsisuru99@gmail.com>
2023-01-05 11:38:53 +0530

    New commits

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+#program to display fibonacci sequence upto n-th term
+nterms=int(input("enter the number of terms: "))
+if(nterms<=0):
+    print("Invalid input")
+else:
+    n1,n2=0,1
+    i=0
+    while(i<nterms):
+        print("{}".format(n1))
+        nth=n1+n2
+        n1=n2
+        n2=nth
+        i=i+1
